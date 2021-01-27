2456e3bc05e6c00f4cbc257976d08c5a0386fc9a
ASinha <69646570+anchalgithub@users.noreply.github.com>
2021-01-27 17:18:21 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,22 @@
+import firebase from 'firebase'
+require('@firebase/firestore')
+
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDeIhbtquqRT0SiOG0OloIIRyiMGgSAsQY",
+    authDomain: "test-97b90.firebaseapp.com",
+    databaseURL: "https://test-97b90-default-rtdb.firebaseio.com",
+    projectId: "test-97b90",
+    storageBucket: "test-97b90.appspot.com",
+    messagingSenderId: "957925011294",
+    appId: "1:957925011294:web:f96af481979151626129cf"
+  };
+
+
+  if(!firebase.apps.length){
+    firebase.initializeApp(firebaseConfig);
+  }
+  
+  export default firebase.firestore();
+
+  
